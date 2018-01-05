8162a43c3154390e46926d21b5c24c95f9512499
Thomas Refis <thomas.refis@gmail.com>
2018-01-05 11:09:00 +0000

    v0.10.116.07+43

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,5 @@
 open Ppx_compare_lib.Builtin
-open Sexplib.Std
+open Ppx_sexp_conv_lib.Conv
 
 type 'a t =
   | A of 'a
@@ -7,52 +7,66 @@ type 'a t =
   | C of 'a t
   | Plop
 [@@deriving_inline sexp, compare]
-let rec t_of_sexp : type a.(Sexplib.Sexp.t -> a) -> Sexplib.Sexp.t -> a t =
+
+
+let rec t_of_sexp : type
+  a.(Ppx_sexp_conv_lib.Sexp.t -> a) -> Ppx_sexp_conv_lib.Sexp.t -> a t =
   let _tp_loc = "test.ml.t"  in
   fun _of_a  ->
     function
-    | Sexplib.Sexp.List ((Sexplib.Sexp.Atom ("a"|"A" as _tag))::sexp_args) as
-      _sexp ->
+    | Ppx_sexp_conv_lib.Sexp.List ((Ppx_sexp_conv_lib.Sexp.Atom
+                                      ("a"|"A" as _tag))::sexp_args) as _sexp ->
       (match sexp_args with
        | v0::[] -> let v0 = _of_a v0  in A v0
-       | _ -> Sexplib.Conv_error.stag_incorrect_n_args _tp_loc _tag _sexp)
-    | Sexplib.Sexp.List ((Sexplib.Sexp.Atom ("b"|"B" as _tag))::sexp_args) as
-      _sexp ->
+       | _ ->
+         Ppx_sexp_conv_lib.Conv_error.stag_incorrect_n_args _tp_loc _tag
+           _sexp)
+    | Ppx_sexp_conv_lib.Sexp.List ((Ppx_sexp_conv_lib.Sexp.Atom
+                                      ("b"|"B" as _tag))::sexp_args) as _sexp ->
       (match sexp_args with
        | v0::[] -> let v0 = list_of_sexp string_of_sexp v0  in B v0
-       | _ -> Sexplib.Conv_error.stag_incorrect_n_args _tp_loc _tag _sexp)
-    | Sexplib.Sexp.List ((Sexplib.Sexp.Atom ("c"|"C" as _tag))::sexp_args) as
-      _sexp ->
+       | _ ->
+         Ppx_sexp_conv_lib.Conv_error.stag_incorrect_n_args _tp_loc _tag
+           _sexp)
+    | Ppx_sexp_conv_lib.Sexp.List ((Ppx_sexp_conv_lib.Sexp.Atom
+                                      ("c"|"C" as _tag))::sexp_args) as _sexp ->
       (match sexp_args with
        | v0::[] -> let v0 = t_of_sexp _of_a v0  in C v0
-       | _ -> Sexplib.Conv_error.stag_incorrect_n_args _tp_loc _tag _sexp)
-    | Sexplib.Sexp.Atom ("plop"|"Plop") -> Plop
-    | Sexplib.Sexp.Atom ("a"|"A") as sexp ->
-      Sexplib.Conv_error.stag_takes_args _tp_loc sexp
-    | Sexplib.Sexp.Atom ("b"|"B") as sexp ->
-      Sexplib.Conv_error.stag_takes_args _tp_loc sexp
-    | Sexplib.Sexp.Atom ("c"|"C") as sexp ->
-      Sexplib.Conv_error.stag_takes_args _tp_loc sexp
-    | Sexplib.Sexp.List ((Sexplib.Sexp.Atom ("plop"|"Plop"))::_) as sexp ->
-      Sexplib.Conv_error.stag_no_args _tp_loc sexp
-    | Sexplib.Sexp.List ((Sexplib.Sexp.List _)::_) as sexp ->
-      Sexplib.Conv_error.nested_list_invalid_sum _tp_loc sexp
-    | Sexplib.Sexp.List [] as sexp ->
-      Sexplib.Conv_error.empty_list_invalid_sum _tp_loc sexp
-    | sexp -> Sexplib.Conv_error.unexpected_stag _tp_loc sexp
+       | _ ->
+         Ppx_sexp_conv_lib.Conv_error.stag_incorrect_n_args _tp_loc _tag
+           _sexp)
+    | Ppx_sexp_conv_lib.Sexp.Atom ("plop"|"Plop") -> Plop
+    | Ppx_sexp_conv_lib.Sexp.Atom ("a"|"A") as sexp ->
+      Ppx_sexp_conv_lib.Conv_error.stag_takes_args _tp_loc sexp
+    | Ppx_sexp_conv_lib.Sexp.Atom ("b"|"B") as sexp ->
+      Ppx_sexp_conv_lib.Conv_error.stag_takes_args _tp_loc sexp
+    | Ppx_sexp_conv_lib.Sexp.Atom ("c"|"C") as sexp ->
+      Ppx_sexp_conv_lib.Conv_error.stag_takes_args _tp_loc sexp
+    | Ppx_sexp_conv_lib.Sexp.List ((Ppx_sexp_conv_lib.Sexp.Atom
+                                      ("plop"|"Plop"))::_) as sexp ->
+      Ppx_sexp_conv_lib.Conv_error.stag_no_args _tp_loc sexp
+    | Ppx_sexp_conv_lib.Sexp.List ((Ppx_sexp_conv_lib.Sexp.List _)::_) as
+      sexp ->
+      Ppx_sexp_conv_lib.Conv_error.nested_list_invalid_sum _tp_loc sexp
+    | Ppx_sexp_conv_lib.Sexp.List [] as sexp ->
+      Ppx_sexp_conv_lib.Conv_error.empty_list_invalid_sum _tp_loc sexp
+    | sexp -> Ppx_sexp_conv_lib.Conv_error.unexpected_stag _tp_loc sexp
 
-let rec sexp_of_t : type a.(a -> Sexplib.Sexp.t) -> a t -> Sexplib.Sexp.t =
+let rec sexp_of_t : type
+  a.(a -> Ppx_sexp_conv_lib.Sexp.t) -> a t -> Ppx_sexp_conv_lib.Sexp.t =
   fun _of_a  ->
     function
     | A v0 ->
-      let v0 = _of_a v0  in Sexplib.Sexp.List [Sexplib.Sexp.Atom "A"; v0]
+      let v0 = _of_a v0  in
+      Ppx_sexp_conv_lib.Sexp.List [Ppx_sexp_conv_lib.Sexp.Atom "A"; v0]
     | B v0 ->
       let v0 = sexp_of_list sexp_of_string v0  in
-      Sexplib.Sexp.List [Sexplib.Sexp.Atom "B"; v0]
+      Ppx_sexp_conv_lib.Sexp.List [Ppx_sexp_conv_lib.Sexp.Atom "B"; v0]
     | C v0 ->
       let v0 = sexp_of_t _of_a v0  in
-      Sexplib.Sexp.List [Sexplib.Sexp.Atom "C"; v0]
-    | Plop  -> Sexplib.Sexp.Atom "Plop"
+      Ppx_sexp_conv_lib.Sexp.List [Ppx_sexp_conv_lib.Sexp.Atom "C"; v0]
+    | Plop  -> Ppx_sexp_conv_lib.Sexp.Atom "Plop"
+
 
 let rec compare : 'a . ('a -> 'a -> int) -> 'a t -> 'a t -> int =
   fun _cmp__a  ->
